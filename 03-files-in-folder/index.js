089913647b59be144632e5583a5a4cd65408f197
NastyaKomar <yukarimoore@icloud.com>
2022-11-04 16:21:12 +0300

    feat:add 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+let path = require('path');
+let fs = require('fs');
+let infoFiles = function (file) {
+  let data = [];
+  if (file.isFile()) {
+    fs.stat(path.resolve(__dirname, 'secret-folder', file.name), function (error, stats) {
+      if (error) {
+        return console.log(error);
+      }
+      data.push(file.name.split('.').slice(0, -1).join('.'));
+      data.push(path.extname(file.name).slice(1));
+      data.push((Math.round(stats.size / 1024)) + 'Kb');
+      console.log(data.join(' - '));
+    });
+  }
+};
+fs.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true }, function (error, files) {
+  if (error) {
+    return console.log(error);
+  }
+  files.forEach(item => {
+    infoFiles(item);
+  });
+});
