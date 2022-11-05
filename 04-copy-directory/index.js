047626294b377c72f8827cffc8d3bf89442f3a93
NastyaKomar <yukarimoore@icloud.com>
2022-11-05 12:39:30 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+let fs = require('fs/promises');
+let path = require('path');
+let files = path.join(__dirname, 'files');
+let filesCopy = path.join(__dirname, 'files-copy');
 
+fs.rm(filesCopy, {
+  recursive: true,
+  force: true
+}).finally(function () {
+  fs.mkdir(filesCopy, {
+    recursive: true
+  });
+  fs.readdir(files, {
+    withFileTypes: true
+  }).then(function (data) {
+    data.forEach(function (item) {
+      if (item.isFile()) {
+        let pathItem = path.join(files, item.name);
+        let pathItemDes = path.join(filesCopy, item.name);
+        fs.copyFile(pathItem, pathItemDes);
+      }
+    });
+  });
+});
